248a4855208d8d33b8ce781e042249480469dc73
Swastik Nath <20301603+swastiknath@users.noreply.github.com>
2019-05-07 20:06:17 +0530

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.CSharp.RuntimeBinder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Globalization;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DataBind
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        public string CurrentTime => DateTime.Now.ToString();
+
+        private DispatcherTimer _timer;
+        public MainPage()
+        {
+            this.InitializeComponent();
+        string CurrentTime = DateTime.Now.ToString();
+            _timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
+            _timer.Tick += (sender, o) =>
+               PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CurrentTime)));
+            _timer.Start();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public interface INotifyPropertyChanged
+    {
+        event PropertyChangedEventHandler PropertyChanged;
+    }
+}
